4be93c80d138689848c2d171c5ad05cf53c64be7
Pooja Thakor <pooja.thakor99@gmail.com>
2019-04-18 12:51:47 +0530

    code with componenet

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li
+      v-for="(input, index) in inputs"
+      :key="`item-${index}`">
+        <input-comp refName="txtEditName" ref="txtEditName" :itemName="input.one" :submitname="submitName"></input-comp>
+        
+        <button @click="deleteRow(index)">Delete</button>
+      </li>
+    </ul>
+     <button @click="addRow">Add row</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import inputComp from './inputComp.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    'input-comp': inputComp
+  },
+  data() {
+    return {
+      inputs: []
+    }
+  },
+  methods: {
+    addRow () {
+      this.inputs.push({
+        one: ''
+      })
+      this.$nextTick(() => {
+        this.$refs.txtEditName.focus()
+      })
+    },
+    deleteRow(index) {
+      this.inputs.splice(index,1)
+    },
+    submitName () {
+      alert('name submitted')
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/inputComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  <input
+    type="text"
+    v-model="itemName"
+    :ref="refName"
+    v-on:keyup.enter="submitname"
+  />
+  
+
+</div>
+</template>
+<script>
+export default {
+  props: ['itemName','refName'],
+  methods: {
+    submitName () {
+      this.$emit('submitname')
+    }
+  }
+}
+</script>
